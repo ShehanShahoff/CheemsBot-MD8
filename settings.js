efa25c5c97e0c36c3532371c67e2402059024fa2
ShehanShahoff <98130194+ShehanShahoff@users.noreply.github.com>
2023-08-10 11:19:58 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['923315062168'] //ur owner number
+global.ownernomer = "923315062168" //ur owner number2
+global.ownername = "Khan Baba Data Provider" //ur owner name
+global.ytname = "YT: Nahi hai Bhai" //ur yt chanel name
+global.socialm = "GitHub: Apko Nahi pta" //ur github or insta name
+global.location = "Pakistani hun" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Khan Baba Data Provider"
+global.ownernumber = '923315062168'
+global.ownername = '🦄드림 가이 Khan Baba'
+global.ownerNumber = ["923315062168@s.whatsapp.net"]
+global.ownerweb = "https://wa.me/923315062168"
+global.websitex = "https://wa.me/923315062168"
+global.wagc = "https://wa.me/923315062168"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "Khan Baba Bot Inc."
+global.botscript = 'https://wa.me/923315062168' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 Khan BABA\n\n"
+global.creator = "923315062168@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
